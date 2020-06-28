64fd6ef870365cd78a43e46c6cdb471128bea07f
Sashini Hettiarachchi <sashinih@99x.lk>
2020-06-28 18:39:36 +0530

    Render md pages as blogs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,39 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
-      <h1>Bolg</h1>
-      <p>This is the blog page</p>
+      <h1>Bolgs</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map(element => {
+          {
+            console.log(element.node)
+          }
+
+          return (
+            <li>
+              <h2>Title : {element.node.frontmatter.title}</h2>
+              <p>Date : {element.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
--- src/posts/gatsby.md
+++ src/posts/gatsby.md
@@ -6,3 +6,7 @@ date: "2019-04-04"
 I just launched a new bootcamp!
 
 #Topics Covered
+
+1.Gatsby
+2.GraphQL
+3.React
